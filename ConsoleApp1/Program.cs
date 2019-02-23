b0a29b10e5a1d7adccc93b994932c23880412daa
chertenokru <admin@chertenok.ru>
2019-02-24 01:30:38 +0300

    fix namespace

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Min
+
 {
     /* Коньков
      * Написать метод, возвращающий минимальное из трёх чисел.
