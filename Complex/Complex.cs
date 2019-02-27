efd2314b72cc7fc765e98f706ae81455e9a411a1
chertenokru <admin@chertenok.ru>
2019-02-28 00:57:57 +0300

    Одна задача вместо 2х

---
+++ Complex/Complex.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    /// <summary>
+    /// Класс реализующий работу с комплексными числами
+    /// </summary>
+        class Complex
+        {
+        /// <summary>
+        /// Мнимая часть
+        /// </summary>
+        private double b;
+        /// <summary>
+        /// вещественная часть
+        /// </summary>
+        private double a;
+
+        public double A { get => a; set => a = value; }
+        public double B { get => b; set => b = value; }
+
+        /// <summary>
+        /// Сложение комплексных чисел
+        /// </summary>
+        /// <param name="a1">Число 1</param>
+        /// <param name="a2">Число 2</param>
+        /// <returns>Результат</returns>
+        public static Complex Plus(Complex a1, Complex a2)
+        {
+            return new Complex(a1.a + a2.a, a1.b + a2.b);
+        }
+        /// <summary>
+        /// Вычитание комплексных числе
+        /// </summary>
+        /// <param name="a1">Число 1</param>
+        /// <param name="a2">Число 2</param>
+        /// <returns>Результат</returns>
+        public static Complex Minus(Complex a1, Complex a2)
+        {
+            return new Complex(a1.a - a2.a, a1.b - a2.b);
+        }
+
+        /// <summary>
+        /// Умножение комплексных чисел
+        /// </summary>
+        /// <param name="a1">Число 1</param>
+        /// <param name="a2">Число 2</param>
+        /// <returns>Результат</returns>
+        public static Complex Multi(Complex a1, Complex a2)
+        {
+            return new Complex(a1.a * a2.a - a1.b *a2.b, a1.b * a2.a + a1.a * a2.b);
+        }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="a">вещественная часть</param>
+        /// <param name="b">мнимая часть</param>
+        public Complex(double a, double b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+
+        /// <summary>
+        /// Визуализация
+        /// </summary>
+        /// <returns></returns>
+            public override string ToString()
+            {
+                return $" ({a} + {b}i)";
+            }
+        }
+    
+}
---
+++ Complex/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+ 
+
+    class Program
+    {
+        /// <summary>
+        /// Осуществляет ввод комплексного числа с проверкой корректности
+        /// </summary>
+        /// <returns>Введенное комплексное число</returns>
+        private static Complex InputComplex()
+        {
+            bool res = false;
+            double a = 0;
+            double b = 0;
+            do
+            {
+                Console.Write("Введите комплексное число через ; - вещественная_часть;мнимая_часть :");
+                string[] str = Console.ReadLine().Split(';');
+                if (str.Length==2)
+                {
+                    res = Double.TryParse(str[0].Replace('.',','), out a);
+                    if (res)
+                    {
+                        res = Double.TryParse(str[1].Replace('.', ','), out b);
+                    }
+
+                }
+                Console.WriteLine((res) ? "ok, следующее число" : "ошибка, повторите ввод");
+            } while (!res);
+            return new Complex(a,b);
+        }
+
+        /// <summary>
+        /// Калькулятор комплексных чисел, выполняет 3 операции *,-,+
+        /// </summary>
+        /// <param name="args">не используется</param>
+        static void Main(string[] args)
+        {
+            bool repeat = false;
+            Complex a1, a2, a3 = null;
+            do {
+                
+                    Console.WriteLine("Выберите операцию -,+,*");
+                    string op = Console.ReadLine();
+
+                repeat = !(op == "-" || op == "+" || op == "*");
+
+                if (!repeat)
+                {
+                    a1 = InputComplex();
+                    a2 = InputComplex();
+                    Console.Write($"{a1} {op} {a2} = ");
+                    switch (op)
+                    {
+                        case "-":
+                            {
+                                a3 = Complex.Minus(a1, a2);
+                                break;
+                            }
+                        case "+":
+                            {
+                                a3 = Complex.Plus(a1, a2);
+                                break;
+                            }
+                        case "*":
+                            {
+                                a3 = Complex.Multi(a1, a2);
+                                break;
+                            }
+
+                    }
+                    Console.WriteLine(a3);
+                    Console.WriteLine("Для продолжения напечатайте y");
+                }
+                else
+                {
+                    Console.WriteLine("Неверно введена операция !");
+                }               
+
+            } while (repeat || Console.ReadLine() == "y");
+
+
+
+        }
+    }
+}
